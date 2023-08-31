9987d8510c6b131eacb71ea2227261a535fdb114
Njaaga-Gakure <njaagagakure@gmail.com>
2023-08-31 14:32:16 +0000

    Validate password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,6 +4,11 @@
 import bcrypt
 
 
-def hash_password(password: str) -> byte:
+def hash_password(password: str) -> bytes:
     """Hash a password."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate a password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
