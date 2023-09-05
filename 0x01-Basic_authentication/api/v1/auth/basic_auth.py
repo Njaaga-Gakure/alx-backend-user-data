3283f732b759743f7bfd3ed9bc4e9310cb6feeab
Njaaga-Gakure <njaagagakure@gmail.com>
2023-09-05 10:46:45 +0000

    Create a class to handle basic access authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""BasicAuth class module."""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """A class for Basic Access Authentication."""
+
+    pass
