aa68d08899668076d8b8bf402c38bc29bfc41338
Njaaga-Gakure <njaagagakure@gmail.com>
2023-09-05 10:59:31 +0000

    Add a method to check the authorization string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,16 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """A class for Basic Access Authentication."""
 
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extract the base64 auth header."""
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+
+        start_str = authorization_header.split(" ")[0]
+        if start_str != "Basic":
+            return None
+
+        return authorization_header.split(" ")[1]
