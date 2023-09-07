4f33ef1d3b40f36d8424f62cccca22434dcfa1e2
Njaaga-Gakure <njaagagakure@gmail.com>
2023-09-07 09:15:37 +0000

    Update before_requests handler

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -48,12 +48,14 @@ def handle_before_request():
     """Handle authorization."""
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth:
         return
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if not auth.authorization_header(request):
+    if (not auth.authorization_header(request)
+            and not auth.session_cookie(request)):
         abort(401)
     if not auth.current_user(request):
         abort(403)
