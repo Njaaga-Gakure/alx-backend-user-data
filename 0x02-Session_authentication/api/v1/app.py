370163804dc123611264f706618fb2f6034439b9
Njaaga-Gakure <njaagagakure@gmail.com>
2023-09-07 08:00:01 +0000

    Create an empty session authentication class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -18,8 +19,10 @@ auth = None
 
 if getenv("AUTH_TYPE") == 'auth':
     auth = Auth()
-elif getenv("AUTH_TYPE") == 'basic_auth':
+if getenv("AUTH_TYPE") == 'basic_auth':
     auth = BasicAuth()
+if getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Session Authntication Module."""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """A class to handle session authentication."""
+
+    pass
