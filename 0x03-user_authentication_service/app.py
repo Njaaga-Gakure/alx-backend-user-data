7433176ad3b77548d00fc52054f2dab8543a36c9
Njaaga-Gakure <njaagagakure@gmail.com>
2023-09-12 15:17:11 +0000

    Implement a route to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,11 @@
 """Flask application."""
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -12,6 +16,18 @@ app = Flask(__name__)
 def index():
     """Index route."""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """Register a user."""
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
