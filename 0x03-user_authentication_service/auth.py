5bbe324342793ae89e2efce6eb834ed2b8595054
Njaaga-Gakure <njaagagakure@gmail.com>
2023-09-12 16:07:39 +0000

    Create a session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,3 +44,13 @@ class Auth:
                                   getattr(user, 'hashed_password'))
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            setattr(user, 'session_id', session_id)
+            return session_id
+        except NoResultFound:
+            return None
